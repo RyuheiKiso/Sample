f8b8e5f98954acebebf9ee9d2db00bbe87386b22
Ryuhei Kiso <uw2374@icloud.com>
2025-06-19 00:27:31 +0900

    feat: 商品およびユーザー機能のサービスインターフェースを実装
    - 商品機能のサービスインターフェース(ProductService)を追加し、HealthCheckメソッドを実装
    - ユーザー機能のサービスインターフェース(UserService)を追加し、HealthCheckメソッドを実装

--- code/backend/internal/product/service.go
+++ code/backend/internal/product/service.go
@@ -1,3 +1,27 @@
 package product
 
-// TODO: 商品機能のサービス実装
+import (
+	"backend/internal/common"
+	"context"
+)
+
+// ProductServiceは商品機能のサービスインターフェース
+type ProductService interface {
+	common.Service
+	// ここに商品固有のメソッドを追加
+}
+
+type productService struct {
+	common.BaseService
+}
+
+func NewProductService() ProductService {
+	return &productService{
+		BaseService: common.BaseService{},
+	}
+}
+
+// HealthCheckの実装（common.Serviceインターフェース）
+func (s *productService) HealthCheck(ctx context.Context) error {
+	return s.BaseService.HealthCheck(ctx)
+}
--- code/backend/internal/user/service.go
+++ code/backend/internal/user/service.go
@@ -1,3 +1,27 @@
 package user
 
-// TODO: ユーザー機能のサービス実装
+import (
+	"backend/internal/common"
+	"context"
+)
+
+// UserServiceはユーザー機能のサービスインターフェース
+type UserService interface {
+	common.Service
+	// ここにユーザー固有のメソッドを追加
+}
+
+type userService struct {
+	common.BaseService
+}
+
+func NewUserService() UserService {
+	return &userService{
+		BaseService: common.BaseService{},
+	}
+}
+
+// HealthCheckの実装（common.Serviceインターフェース）
+func (s *userService) HealthCheck(ctx context.Context) error {
+	return s.BaseService.HealthCheck(ctx)
+}
